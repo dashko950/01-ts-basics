3030e879691f4a83b94d684c13535f080be8089c
Danylo <dashko950@gmail.com>
2026-02-27 16:19:54 +0100

    ends2

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,10 +2,8 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-// Явна типізація дженериків у викликах
-console.log(getFirstElement<number>([1, 2, 3])); // 1
-console.log(getFirstElement<string>(["a", "b", "c"])); // "a"
-console.log(getFirstElement<boolean>([true, false, true])); // true
+console.log(getFirstElement<number>([1, 2, 3])); // 1 (number)
+console.log(getFirstElement<string>(["a", "b", "c"])); // "a" (string)
+console.log(getFirstElement<boolean>([true, false, true])); // true (boolean)
 
-// Якщо передати масив змішаного типу без вказівки дженерика – TypeScript виведе об'єднаний тип (union)
-const mixed = getFirstElement([1, "a"]); // тип повернення string | number
+console.log(getFirstElement<number | string>([1, "a"])); // 1 (number | string)
