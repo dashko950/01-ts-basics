8723de045ab86f87a96ec836bb7c31b7a533cbf7
Danylo <dashko950@gmail.com>
2026-02-27 16:22:24 +0100

    asdsd

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,8 +2,15 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-console.log(getFirstElement<number>([1, 2, 3])); // 1 (number)
-console.log(getFirstElement<string>(["a", "b", "c"])); // "a" (string)
-console.log(getFirstElement<boolean>([true, false, true])); // true (boolean)
+// Явна типізація дженериків у викликах функцій
+const number1 = getFirstElement<number>([1, 2, 3]); // 1 (тип number)
+const string1 = getFirstElement<string>(["a", "b", "c"]); // "a" (тип string)
+const boolean1 = getFirstElement<boolean>([true, false, true]); // true (тип boolean)
 
-console.log(getFirstElement<number | string>([1, "a"])); // 1 (number | string)
+// Вивід результатів для перевірки
+console.log(number1); // 1
+console.log(string1); // "a"
+console.log(boolean1); // true
+
+const mixed = getFirstElement<number | string>([1, "a"]); // 1 (тип number | string)
+console.log(mixed); // 1
